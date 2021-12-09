6cd8f69de0c26783385f0354cecb54b5210112dd
old-bin <353016323@qq.com>
2021-12-10 02:25:46 +0800

    feat: complete account test

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { Switch, Router, Route, Redirect } from 'react-router-dom';
 import Index from '@/pages/index/index'
 import Account from '@/pages/account/index'
+import Account2 from '@/pages/account2/index'
 // import Loading from '@/components/Loading';
 
 const Routes = () => (
@@ -9,6 +10,7 @@ const Routes = () => (
     <Switch>
       < Route exact path='/' component={Index} />
       < Route exact path='/account' component={Account} />
+      < Route exact path='/accountTest' component={Account2} />
       <Redirect from="*" to='/' />
     </Switch>
   </Suspense>
