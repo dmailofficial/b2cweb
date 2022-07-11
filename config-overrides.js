d41b35c9df9b284d625f811f0500d34937c8ea76
old-bin <353016323@qq.com>
2022-07-11 17:15:58 +0800

    big js split when build

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,19 @@
-const { override, overrideDevServer, fixBabelImports, addWebpackAlias, addWebpackModuleRule, setWebpackOptimizationSplitChunks, addBundleVisualizer } = require('customize-cra');
+const {
+  override,
+  overrideDevServer,
+  fixBabelImports,
+  addWebpackAlias,
+  addWebpackModuleRule,
+  setWebpackOptimizationSplitChunks,
+  addBundleVisualizer,
+} = require("customize-cra");
 
-const path = require('path')
-const resolve = dir => path.join(__dirname, '.', dir)
+const path = require("path");
+const resolve = (dir) => path.join(__dirname, ".", dir);
 
-const removeManifest = () => config => {
+const removeManifest = () => (config) => {
   config.plugins = config.plugins.filter(
-      p => p.constructor.name !== "ManifestPlugin"
+    (p) => p.constructor.name !== "ManifestPlugin"
   );
   return config;
 };
@@ -20,24 +28,56 @@ const addProxy = () => (configFunction) => {
   // };
 
   return configFunction;
-}
+};
 
-const closedMap = config => {
-   config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
-   return config;
- }; 
+const closedMap = (config) => {
+  config.devtool =
+    config.mode === "development" ? "cheap-module-source-map" : false;
+  return config;
+};
 
 module.exports = {
   webpack: override(
-      addBundleVisualizer({}, true),
-      removeManifest(),
-      addWebpackAlias({
-        ['@']: resolve('src')
-      }),
-      closedMap
-      // addLessLoader(),
+    addBundleVisualizer({}, true),
+    removeManifest(),
+    addWebpackAlias({
+      ["@"]: resolve("src"),
+    }),
+    setWebpackOptimizationSplitChunks({
+      // chunks: "all",
+      // minSize: 0,
+      cacheGroups: {
+        dlleth: {
+          test: /@ethereumjs/,
+          name: "ethereumjs",
+          chunks: "all",
+          priority: 4,
+          enforce: true,
+        },
+        dllethothers: {
+          test: /eth.*/,
+          name: "ethother",
+          chunks: "all",
+          priority: 3,
+          enforce: true,
+        },
+        dllpolymers: {
+          test: /(idna.*|@mui|browserify-sign|bignumber|elliptic|aes|keccak|react.*).*/,
+          name: "dll-polymers",
+          chunks: "all",
+          priority: 3,
+          enforce: true,
+        },
+        commons: {
+          chunks: "all",
+          name: "commons",
+          minChunks: 2,
+          reuseExistingChunk: true,
+        },
+      },
+    }),
+    closedMap
+    // addLessLoader(),
   ),
-  devServer: overrideDevServer(
-    addProxy()
-  )
-}
+  devServer: overrideDevServer(addProxy()),
+};
